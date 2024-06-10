9aaac788d272d798c69d85bf70d10ef5e4eb9ae1
Vyankateshwar Taikar <102132721+VyankateshwarTaikar@users.noreply.github.com>
2024-06-10 10:11:03 +0530

    Update config.tf

--- terraform/config.tf
+++ terraform/config.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-west-2"
-  access_key = ""
-  secret_key = ""
+  access_key = "AKIA5O32HPW6Q3PGLE6M"
+  secret_key = "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"
 } 
